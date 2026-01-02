163f7b29c05a7db824f72d48e33f977260585677
HardBoss07 <matteo@bosshard.net>
2026-01-02 17:43:59 +0100

    initial CLI skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "project-structure")]
+struct Cli {
+    /// Root path of the project
+    path: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let root = cli.path.unwrap_or_else(|| ".".into());
+
+    println!("Project root: {}", root.display());
+    Ok(())
 }
