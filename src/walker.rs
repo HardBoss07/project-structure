118c66eed3005157d0d8f77e69a80f8de2dd5dea
HardBoss07 <matteo@bosshard.net>
2026-01-03 10:44:41 +0100

    refactor: using ignore crate fro filesystem traversal

--- src/walker.rs
+++ src/walker.rs
@@ -1,22 +1,21 @@
 use crate::config::Config;
 use anyhow::Result;
-use std::fs;
+use ignore::WalkBuilder;
 use std::path::PathBuf;
 
 pub fn walk(config: &Config) -> Result<Vec<PathBuf>> {
-    let mut entries = Vec::new();
-    visit_dir(&config.root, &mut entries)?;
-    Ok(entries)
-}
+    let mut paths = Vec::new();
 
-fn visit_dir(path: &PathBuf, entries: &mut Vec<PathBuf>) -> Result<()> {
-    if path.is_dir() {
-        for entry in fs::read_dir(path)? {
-            let entry = entry?;
-            let entry_path = entry.path();
-            entries.push(entry_path.clone());
-            visit_dir(&entry_path, entries)?;
-        }
+    let walker = WalkBuilder::new(&config.root)
+        .git_ignore(true)
+        .git_global(true)
+        .git_exclude(true)
+        .build();
+
+    for entry in walker {
+        let entry = entry?;
+        paths.push(entry.path().to_path_buf());
     }
-    Ok(())
+
+    Ok(paths)
 }
